d82e515b278fe81da787a5876785b10306fc2036
kojima <>
2019-01-10 20:13:10 +0900

    テストテキスト出力

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -40,6 +40,7 @@ class FaceDetector(object):
         # 顔の位置を描画する
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.puttext(frame,test,(x,y),,12,(255,0,0))
 
         # frameを戻り値として返す
         return frame
